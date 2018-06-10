1c1dc34d7d81a99023e54a174c01a9e0957e1b3e
Muata Nkosi <mnkosi@fandm.edu>
2018-06-10 18:45:56 -0400

    add hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,13 +12,22 @@ export class HeroesComponent implements OnInit {
 
   constructor(private heroService:HeroService) { }
 
-  ngOnInit() {
+  ngOnInit(): void{
     this.getHeroes();
   }
 
-  getHeroes():void{
+  getHeroes(): void{
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string): void{
+    name = name.trim();
+    if (!name) {return;}
+    this.heroService.addHero({name} as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
   }
 
 }
