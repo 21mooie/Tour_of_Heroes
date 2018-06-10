ac0a3763d09b65c3ac386699497af5708fe357c6
Muata Nkosi <mnkosi@fandm.edu>
2018-06-10 18:06:39 -0400

    heroes error logging

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -26,8 +26,12 @@ export class HeroService {
   }
 
   getHero(id: number): Observable<Hero>{
-    this.messageService.add(`Hero Service: fetched hero id=${id}`);
-    return of (HEROES.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url)
+      .pipe(
+        tap(_ => this.log(`fetched hero id=${id}`)),
+        catchError(this.handleError<Hero>(`getHero id=${id}`))
+      );
   }
 
   private log(message: string) {
