2ff866c91d0548af9ca52d381520512f95724f2f
Muata Nkosi <mnkosi@fandm.edu>
2018-06-15 12:10:37 -0400

    displaying data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <h1>{{title}}</h1>
+    <h2>My favorite hero is {{myHero.name}}</h2>
+    <ul> These are all the heroes.
+      <span *ngIf="heroes.length > 3"> There are multiple heroes here. </span>
+      <li *ngFor="let hero of heroes">{{hero.name}}</li>
+    </ul>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Tour of Heroes';
+  heroes : Hero[] = [
+    new Hero(1, 'Windstorm'),
+    new Hero(13, 'Bombasto'),
+    new Hero(15, 'Magneta'),
+    new Hero(20, 'Tornado')
+  ];
+  myHero : Hero = this.heroes[0];
 }
+
